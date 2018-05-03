8fb2c102203cbf97d63a603eac5990c07679835c
HjorturJ <hjorturv17@ru.is>
2018-05-03 18:42:30 +0000

    Login and Register ViewModels added

--- BookCave/Controllers/AccountController.cs
+++ BookCave/Controllers/AccountController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
 using BookCave.Models;
 using BookCave.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -23,8 +25,52 @@ namespace BookCave.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterViewModel register)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if(result.Succeeded)
+            {
+                await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
+                await _signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
             return View();
         }
     }
---
+++ BookCave/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookCave.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
