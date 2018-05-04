e1cb4a1241ecd5a1af6638ce7370a99300022787
HjorturJ <hjorturv17@ru.is>
2018-05-04 14:08:16 +0000

    Database initial migration with --context

--- BookCave/Data/EntityModels/Book.cs
+++ BookCave/Data/EntityModels/Book.cs
@@ -2,7 +2,7 @@ namespace BookCave.Data.EntityModels
 {
     public class Book
     {
-        private int Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string ImageLink { get; set; }
         public string Description { get; set; }
---
+++ BookCave/Migrations/20180504135535_initialMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    ImageLink = table.Column<string>(nullable: true),
+                    NumberOfUserRating = table.Column<int>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    UserRatingAvg = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
