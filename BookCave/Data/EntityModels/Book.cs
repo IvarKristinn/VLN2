e150cdba749f236fc2f948fbffb2ffe02b25c10d
Birkir K <birkir17@ru.is>
2018-05-04 13:00:55 +0000

    Book.cs

--- BookCave/Data/EntityModels/Book.cs
+++ BookCave/Data/EntityModels/Book.cs
@@ -2,6 +2,15 @@ namespace BookCave.Data.EntityModels
 {
     public class Book
     {
-        
+        private int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageLink { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int UserRatingAvg { get; set; }
+        public int NumberOfUserRating { get; set; }
+        public double Price { get; set; }
+        public string Author { get; set; }
+        //public List<CommentsId> CommentIdList { get; set; }
     }
 }
