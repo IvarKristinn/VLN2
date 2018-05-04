e4de35dc4f6d84cb8adc034274a13571f31d480d
Birkir K <birkir17@ru.is>
2018-05-04 15:08:15 +0000

    Gerdi seed fallið og testaði DB

--- BookCave/Program.cs
+++ BookCave/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BookCave.Data;
+using BookCave.Data.EntityModels;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,12 +16,29 @@ namespace BookCave
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            //SeedData();
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        public static void SeedData()
+        {
+            var db = new DataContext();
+            
+            if(!db.Books.Any())
+            {
+                var initialBooks = new List<Book>()
+                {
+                    new Book { Title = "Grey Sister"},
+                    new Book { Title = "The Neon Boneyard"}
+                };
+                db.AddRange(initialBooks);
+                db.SaveChanges();
+            }
+        }
     }
 }
--- BookCave/Repositories/BookRepo.cs
+++ BookCave/Repositories/BookRepo.cs
@@ -1,13 +1,25 @@
 using System.Collections.Generic;
+using BookCave.Data;
 using BookCave.Models.ViewModels;
+using System.Linq;
 
 namespace BookCave.Repositories
 {
     public class BookRepo
     {
+        private DataContext _db;
+        public BookRepo()
+        {
+            _db = new DataContext();
+        }
         public List<BookThumbnailViewModel> GetBooksByTitle()
         {
-            return null;
+            var books =   (from b in _db.Books
+                            select new BookThumbnailViewModel
+                            {
+                                Title = b.Title
+                            }).ToList();
+            return books;
         }
     }
 }
