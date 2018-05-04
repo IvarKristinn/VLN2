21fdebdb48f01ce52b031c0775691ac87f4722fc
Birkir K <birkir17@ru.is>
2018-05-04 14:07:59 +0000

    Repo og service möppur

---
+++ BookCave/Repositories/BookRepo.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using BookCave.Models.ViewModels;
+
+namespace BookCave.Repositories
+{
+    public class BookRepo
+    {
+        public List<BookThumbnailViewModel> GetBooksByTitle()
+        {
+            return null;
+        }
+    }
+}
