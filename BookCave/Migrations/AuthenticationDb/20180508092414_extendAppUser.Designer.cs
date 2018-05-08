96c186061c487acbfdf5b7f0d104e3db4cbd0dbf
HjorturJ <hjorturv17@ru.is>
2018-05-08 10:14:38 +0000

    Users can add a book to their profile as favorite book

--- BookCave/Migrations/AuthenticationDb/20180508092414_extendAppUser.Designer.cs
+++ BookCave/Migrations/AuthenticationDb/20180508092414_extendAppUser.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BookCave.Migrations.AuthenticationDb
 {
     [DbContext(typeof(AuthenticationDbContext))]
-    partial class AuthenticationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180508092414_extendAppUser")]
+    partial class extendAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
