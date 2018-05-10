289a3d61acdc9f0f61c85c8aa7acd82994b00560
HjorturJ <hjorturv17@ru.is>
2018-05-10 17:27:54 +0000

    now we dont send in an empty review if user leaves a rating but no review

--- BookCave/Controllers/BookController.cs
+++ BookCave/Controllers/BookController.cs
@@ -86,7 +86,7 @@ namespace BookCave.Controllers
             var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
             bool ReviewSuccess = true;
             //debug to see what value of review is when you leave it empty
-            if(review != "")
+            if(review != null)
             {
                 ReviewSuccess = _bookService.AddReview(userId, bookId,  review);
             }
